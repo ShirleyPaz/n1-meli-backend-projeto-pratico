07beff7380eda69bb8aca8dfaf03dbca5f5898fc
shirleypaz <shirley.pazz@gmail.com>
2019-10-22 20:54:29 -0300

    GetCompletedToDos and GetToDosByCollaborator methods created

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -7,8 +7,29 @@ exports.get = (req, res) => {
 
 exports.getById = (req, res) => {
     const id = req.params.id
-    if (id < 1 || id > 17) {
-       // Se não encontrar
+    const tarefaById = tarefas.find(tarefa => tarefa.id == id)
+    if(!tarefaById) {
+        res.status(200).send('Não há tarefa registrada com este id.')
     }
-    res.status(200).send(tarefas.find(tarefa => tarefa.id == id))
+    res.status(200).send(tarefaById);
+}
+
+exports.getCompletedToDos = (req, res) => {
+    const tarefasConcluidas = tarefas.filter(tarefa => tarefa.concluido == 'true');
+    if (tarefasConcluidas.length == 0) {
+        res.status(200).send('Não há tarefas concluídas.')
+    } else {
+        res.status(200).send(tarefasConcluidas);
+    }
+}
+
+exports.getToDosByCollaborator = (req, res) => {
+    const nome = req.params.nome;
+    const tarefasFiltradas = tarefas.filter( tarefa =>  tarefa.colaborador.toUpperCase() == nome.toUpperCase());
+
+    if (tarefasFiltradas.length == 0) {
+        res.status(200).send('Não há tarefas para este colaborador.')
+    } else {
+        res.status(200).send(tarefasFiltradas);
+    }
 }
